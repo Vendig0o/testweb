fc860e5d5f6a2a9ff7dd53bceb99958552a3ef47
Vendig0o <blokhin_evgen1y@mail.ru>
2025-01-12 22:30:30 +0300

    Работа с выпадающим списком

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -61,3 +61,7 @@ class LoginPageHelper(BasePage):
     def click_recovery(self):
         self.attach_screenshot()
         self.find_element(LoginPageLocators.PROFILE_RECOVERY_BUTTON).click()
+
+    @allure.step("Переходим к регистрации")
+    def click_registration(self):
+        self.find_element(LoginPageLocators.REGISTER_BUTTON).click()
